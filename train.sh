ece661c9ba2e437ad9ad4b5e2934378cdae9648c
VedantDere0104 <76057253+VedantDere0104@users.noreply.github.com>
2023-07-29 21:24:11 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,14 @@
 ################## Faces #############################
 
-python ./scripts/train.py   --dataset_type='ffhq_frontalization'  --start_from_latent_avg \
---id_lambda=1.1  --val_interval=200000 --save_interval=10000 --max_steps=100000  --stylegan_size=1024 --is_train=True \
---distortion_scale=0.15 --aug_rate=0.9 --res_lambda=0.5  \
---stylegan_weights='./pretrained/stylegan2-ffhq-config-f.pt' --checkpoint_path "/content/drive/MyDrive/HFGI_New/HFGI/experiment/ffhq_frontalization/checkpoints/iteration_20000.pt" \
---workers=48  --batch_size=16  --test_batch_size=1 --test_workers=48 --exp_dir='./experiment/ffhq_frontalization' \
---lpips_lambda 0.1 --l2_lambda 0.2
+python ./scripts/train.py   --dataset_type='ffhq_encode'  --start_from_latent_avg \
+--val_interval=200000 --save_interval=10000 --max_steps=100000  --stylegan_size=1024 --is_train=True \
+--distortion_scale=0.15 --aug_rate=0.9  \
+--stylegan_weights='./pretrained/stylegan2-ffhq-config-f.pt' --checkpoint_path "ckpt" \
+--workers=48  --batch_size=16  --test_batch_size=1 --test_workers=48 --exp_dir='./experiment/ffhq' \
 
 
 # python ./scripts/train.py   --dataset_type='cars_encode'  --start_from_latent_avg \
-# --id_lambda=0.1  --val_interval=200000 --save_interval=10000 --max_steps=100000  --stylegan_size=512 --is_train=True \
-# --distortion_scale=0.15 --aug_rate=0.9 --res_lambda=0.1  \
-# --stylegan_weights='/content/drive/MyDrive/HFGI_New/HFGI/pretrained/stylegan2-car-config-f.pt' --checkpoint_path='/content/drive/MyDrive/HFGI/HFGI/experiment/cars_encode/checkpoints/iteration_90000.pt'  \
+#  --val_interval=200000 --save_interval=10000 --max_steps=100000  --stylegan_size=512 --is_train=True \
+# --distortion_scale=0.15 --aug_rate=0.9  \
+# --stylegan_weights='./pretrained/stylegan2-car-config-f.pt' --checkpoint_path='ckpt_cars'  \
 # --workers=48  --batch_size=4  --test_batch_size=1 --test_workers=48 --exp_dir='./experiment/cars_encode' 
